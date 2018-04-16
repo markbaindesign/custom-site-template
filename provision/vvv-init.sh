5396159439b9e52cc85b972bc16f35efd56f2fda
markbain <markbaindesign@gmail.com>
2018-04-16 18:06:01 +0200

    add comments

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -9,6 +9,7 @@ WP_TYPE=`get_config_value 'wp_type' "single"`
 DB_NAME=`get_config_value 'db_name' "${VVV_SITE_NAME}"`
 DB_NAME=${DB_NAME//[\\\/\.\<\>\:\"\'\|\?\!\*-]/}
 
+echo -e "\nStart provisioning '${DOMAIN}'"
 # Make a database, if we don't already have one
 echo -e "\nCreating database '${DB_NAME}' (if it's not already there)"
 mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME}"
